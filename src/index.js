24c1002d5b3e2c238fd343fb018fef9232613880
Pratyush Kumar <pratyushkumartheking@gmail.com>
2025-08-20 15:50:31 +0530

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 // Note: In a real environment, you would install bootstrap: npm install bootstrap
 // and import it like this:
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
